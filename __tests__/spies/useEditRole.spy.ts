b2801c3567583ddebb6a2a5c6e48596ad7447656
Csete Balázs <csetebalu@gmail.com>
2022-05-17 16:17:17 +0200

    edit role

---
+++ __tests__/spies/useEditRole.spy.ts
@@ -0,0 +1,20 @@
+import * as useEditRole from "../../src/components/[guild]/RolesByPlatform/components/RoleListItem/components/EditRole/hooks/useEditRole"
+
+const useEditRoleSpy = vi.spyOn(useEditRole, "default")
+const onSubmitSpy = vi.fn()
+
+beforeEach(() => {
+  useEditRoleSpy.mockReturnValue({
+    onSubmit: ({ name, description }) => {
+      console.log({ name, description })
+      onSubmitSpy({ name, description })
+    },
+    isLoading: false,
+    isSigning: false,
+    response: null,
+    error: null,
+  })
+})
+
+export { onSubmitSpy }
+export default useEditRoleSpy
